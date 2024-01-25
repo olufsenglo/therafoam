bc55fab497fd24915b9e6be7f0680398ca49bbc1
Glo <olsheaolufsen@gmail.com>
2024-01-25 18:49:25 +0100

    Add Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   theme: {
     extend: {
 	fontFamily: {
-		sans: ["Inter"]
+		sans: ["Inter", "Arial", "sans-serif", "system-ui", "ui-sans-serif"],
+		serif: ["Playfair Display", "Georgia", "ui-serif", "serif"]
+	},
+	colors: {
+		primary: "#003d35",
+		secondary: "#d2de32",
+		dark: "#1b1e32",
+		gray: "#3f4242"
 	}
     },
   },
